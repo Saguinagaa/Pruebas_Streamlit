1fb0a38dda14724188ab73af570e7603532f0cc3
Sebastian Aguinaga Velásquez <saguinagavelasquez@gmail.com>
2023-11-17 12:09:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-def calculate_amortization(interest_rate, months, loan_amount, additional_payment_month, additional_payment):
+def calculate_amortization(interest_rate, months, loan_amount):
     # Calculating the monthly interest rate
     monthly_interest_rate = interest_rate / 12 / 100
     
@@ -15,11 +16,6 @@ def calculate_amortization(interest_rate, months, loan_amount, additional_paymen
     for month in range(1, months + 1):
         interest = remaining_balance * monthly_interest_rate
         principal = monthly_payment - interest
-        
-        # Check if the current month matches the additional payment month
-        if month == additional_payment_month:
-            remaining_balance -= additional_payment  # Reduce the remaining balance by the additional payment
-            
         remaining_balance -= principal
         
         amortization_schedule = amortization_schedule._append({
@@ -40,12 +36,8 @@ interest_rate = st.number_input('Tasa de interés anual (%)', min_value=0.01, va
 months = st.number_input('Número de meses', min_value=1, value=12, step=1)
 loan_amount = st.number_input('Monto del préstamo', min_value=1.0, value=1000.0, step=1.0)
 
-additional_payment_month = st.number_input('Mes de pago adicional', min_value=1, value=1, step=1)
-additional_payment = st.number_input('Pago adicional', min_value=0.0, value=0.0, step=1.0)
-
 if st.button('Calcular'):
-    monthly_payment, amortization_table = calculate_amortization(interest_rate, months, loan_amount,
-                                                                additional_payment_month, additional_payment)
+    monthly_payment, amortization_table = calculate_amortization(interest_rate, months, loan_amount)
     
     st.subheader('Detalles del Préstamo')
     st.write(f'Monto del préstamo: {loan_amount}')
@@ -55,3 +47,4 @@ if st.button('Calcular'):
     
     st.subheader('Tabla de Amortización')
     st.write(amortization_table)
+
