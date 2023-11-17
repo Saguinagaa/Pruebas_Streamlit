50b8e7b2e8cc55c392f9039e656f8f2e8be04843
Sebastian Aguinaga Velásquez <saguinagavelasquez@gmail.com>
2023-11-16 23:00:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,46 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-def calculate_amortization(interest_rate, months, loan_amount):
-    # Calculating the monthly interest rate
-    monthly_interest_rate = interest_rate / 12 / 100
+def nominal_a_efectiva(tasa_nominal, periodo_capitalizacion):
+    if periodo_capitalizacion == 'Mensual':
+        n = 12
+    elif periodo_capitalizacion == 'Trimestral':
+        n = 4
+    elif periodo_capitalizacion == 'Semestral':
+        n = 2
+    else:  # Anual
+        n = 1
     
-    # Calculating the monthly payment using the formula for an amortizing loan
-    monthly_payment = (loan_amount * monthly_interest_rate) / (1 - (1 + monthly_interest_rate) ** -months)
-    
-    # Creating a DataFrame for the amortization schedule
-    amortization_schedule = pd.DataFrame(columns=['Month', 'Payment', 'Principal', 'Interest', 'Remaining Balance'])
-    remaining_balance = loan_amount
-    
-    for month in range(1, months + 1):
-        interest = remaining_balance * monthly_interest_rate
-        principal = monthly_payment - interest
-        remaining_balance -= principal
-        
-        amortization_schedule = amortization_schedule._append({
-            'Month': month,
-            'Payment': monthly_payment,
-            'Principal': principal,
-            'Interest': interest,
-            'Remaining Balance': remaining_balance
-        }, ignore_index=True)
-    
-    return monthly_payment, amortization_schedule
+    tasa_efectiva = n * ((1 + tasa_nominal) ** (1 / n) - 1)
+    return tasa_efectiva
 
-# Streamlit app
-st.title('Calculadora de Préstamos y Amortización')
-
-# User input for loan details
-interest_rate = st.number_input('Tasa de interés anual (%)', min_value=0.01, value=5.0, step=0.01)
-months = st.number_input('Número de meses', min_value=1, value=12, step=1)
-loan_amount = st.number_input('Monto del préstamo', min_value=1.0, value=1000.0, step=1.0)
-
-if st.button('Calcular'):
-    monthly_payment, amortization_table = calculate_amortization(interest_rate, months, loan_amount)
+def efectiva_a_nominal(tasa_efectiva, periodo_capitalizacion):
+    if periodo_capitalizacion == 'Mensual':
+        n = 12
+    elif periodo_capitalizacion == 'Trimestral':
+        n = 4
+    elif periodo_capitalizacion == 'Semestral':
+        n = 2
+    else:  # Anual
+        n = 1
     
-    st.subheader('Detalles del Préstamo')
-    st.write(f'Monto del préstamo: {loan_amount}')
-    st.write(f'Tasa de interés anual: {interest_rate}%')
-    st.write(f'Número de meses: {months}')
-    st.write(f'Cuota mensual: {monthly_payment:.2f}')
-    
-    st.subheader('Tabla de Amortización')
-    st.write(amortization_table)
+    tasa_nominal = ((1 + tasa_efectiva / n) ** n) - 1
+    return tasa_nominal
+
+st.title('Conversor de Tasas de Interés')
+
+tipo_conversion = st.selectbox('Seleccione el tipo de conversión:', ['Nominal a Efectiva', 'Efectiva a Nominal'])
+
+if tipo_conversion == 'Nominal a Efectiva':
+    tasa_nominal = st.number_input('Ingrese la tasa de interés nominal:', min_value=0.0, value=0.0, step=0.01)
+    periodo_capitalizacion = st.selectbox('Seleccione el periodo de capitalización:', ['Mensual', 'Trimestral', 'Semestral', 'Anual'])
+
+    if st.button('Convertir'):
+        tasa_efectiva = nominal_a_efectiva(tasa_nominal, periodo_capitalizacion)
+        st.write(f"La tasa de interés efectiva es: {tasa_efectiva:.4f}")
+else:
+    tasa_efectiva = st.number_input('Ingrese la tasa de interés efectiva:', min_value=0.0, value=0.0, step=0.01)
+    periodo_capitalizacion = st.selectbox('Seleccione el periodo de capitalización:', ['Mensual', 'Trimestral', 'Semestral', 'Anual'])
+
+    if st.button('Convertir'):
+        tasa_nominal = efectiva_a_nominal(tasa_efectiva, periodo_capitalizacion)
+        st.write(f"La tasa de interés nominal es: {tasa_nominal:.4f}")
